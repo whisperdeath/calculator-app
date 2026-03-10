42e7186a690d0e251f1cfa14b56b7e9001265518
Aya <aya.adadii@gmail.com>
2026-03-10 11:55:47 +0000

    Mods

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.post('/github-webhook', (req, res) => {
     console.log("Webhook received!");
     console.log(req.body);
 
-    res.status(200).send("Webhook received");
+    res.status(200).send("Webhook received !");
 });
 
 const PORT = 3000;
