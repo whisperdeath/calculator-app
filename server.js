f4472afa937786b3b04e62c0fe79f4da1c58494a
Aya <aya.adadii@gmail.com>
2026-03-10 11:26:27 +0000

    Modifications

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// GitHub webhook endpoint
+app.post('/github-webhook', (req, res) => {
+    console.log("Webhook received!");
+    console.log(req.body);
+
+    res.status(200).send("Webhook received");
+});
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
